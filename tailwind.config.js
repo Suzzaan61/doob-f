b8f61b38fc6d1c4b217e8ec2515928c60a57cf8b
suzan <sznghimire61@gmail.com>
2023-07-01 00:16:48 +0545

    first

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    screens: {
+      xm:'375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      lgn:'1106px', //For navigation bar
+      xl: '1280px',
+      '2xl': '1536px',
+    }
+  },
+  
+  plugins: [],
+}
+
